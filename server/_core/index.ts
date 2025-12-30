82db4bf89961fb1132ee4f5b59ec39998bdb1c99
Clinton <clinton.vermaak@gmail.com>
2025-12-30 17:27:38 +0200

    Remove OAuth from index.ts

--- server/_core/index.ts
+++ server/_core/index.ts
@@ -3,6 +3,7 @@ import express from "express";
 import { createServer } from "http";
 import net from "net";
 import { createExpressMiddleware } from "@trpc/server/adapters/express";
+import { authMiddleware } from './auth'
 //import { registerOAuthRoutes } from "./oauth";
 import { appRouter } from "../routers";
 import { createContext } from "./context";
