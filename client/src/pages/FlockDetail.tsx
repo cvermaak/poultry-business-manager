c9e921b7b685a756a7960bf0e9ca24e844c54c3c
Clinton <clinton.vermaak@gmail.com>
2026-01-10 10:16:39 +0200

    Update FlockDetails.tsx to resolve gram/kg value

--- client/src/pages/FlockDetail.tsx
+++ client/src/pages/FlockDetail.tsx
@@ -363,7 +363,7 @@ export default function FlockDetail() {
         actuals.find(a => a.day === day)?.weightKg ?? null,
       feedKg:
 		actuals.find(a => a.day === day)?.feedKg
-		? actuals.find(a => a.day === day)!.feedKg * 1000
+		? actuals.find(a => a.day === day)!.feedKg ?? null,
 		: null,
     };
 
